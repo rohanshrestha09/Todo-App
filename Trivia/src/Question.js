473ac1ba0d64df1e7f4b63d53e7331a503b0d76c
rohanshrestha09 <87956956+rohanshrestha09@users.noreply.github.com>
2022-04-30 16:41:08 +0545

    Delete Trivia directory

--- Trivia/src/Question.js
+++
@@ -1,9 +0,0 @@
-const Question = (props) => {
-    return (
-        <div className='questionSection'>
-            <div>{props.database[props.index].id}</div>
-        </div>
-    )
-}
-
-export default Question
